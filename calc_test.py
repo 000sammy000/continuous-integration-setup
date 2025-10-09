ace87e43449f563ac51a2433ed7d596ad7c4f7ae
黃芷柔 <120660953+000sammy000@users.noreply.github.com>
2025-10-09 10:58:09 +0800

    Update calc_test.py

--- calc_test.py
+++ calc_test.py
@@ -1,24 +1,31 @@
 import unittest
 from Calc import Calculator  # The class we are going to implement
 
+
 class TestCalculator(unittest.TestCase):
+
+
     def test_add(self):
         calc = Calculator()
         result = calc.add(2, 3)
         self.assertEqual(result, 5)  # Expect 2 + 3 = 5
 
+
     def test_subtract(self):
         calc = Calculator()
         self.assertEqual(calc.subtract(5, 3), 2)
+
 
     def test_multiply(self):
         calc = Calculator()
         self.assertEqual(calc.multiply(4, 3), 12)
 
+
     def test_divide(self):
         calc = Calculator()
         self.assertEqual(calc.divide(10, 3), 3)
-    
+
+
     def test_divide_by_zero(self):
         calc = Calculator()
         with self.assertRaises(ValueError) as context:
